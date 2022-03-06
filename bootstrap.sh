506003491e91f46da560247b8a61e5d526dda897
Martin <martin11lrx@gmail.com>
2022-03-06 21:30:43 +0000

    Added weave-net CNI

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,5 @@
 
 set -euo pipefail
 
-helm repo add "cilium" "https://helm.cilium.io/"
-helm repo update
-
-# cilium
-CILIUM_VERSION=1.11.2 
-helm upgrade --install cilium cilium/cilium \
-  --version $CILIUM_VERSION \
-  --values helm/values/cilium.yaml \
-  --namespace kube-system
+# cni
+helm upgrade --install weave-net ./helm/charts/weave-net -n kube-system
