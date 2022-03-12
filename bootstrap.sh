1de4d601c79683254a6a648c7806bd43a9b2fa69
Martín Montes <martin11lrx@gmail.com>
2022-03-12 14:57:46 +0100

    Added kube-prometheus-stack

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,29 @@
 
 set -euo pipefail
 
+helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo add metallb https://metallb.github.io/metallb
 helm repo update
+
+kubectl apply -f manifests/secrets
 
 # cni
 helm upgrade --install weave-net ./helm/charts/weave-net -n kube-system
 
-# metallb
+# kube-prometheus-stack
+KUBE_PROMETHEUS_STACK_VERSION=33.2.0
 
-# enable strict ARP
+helm upgrade --install kube-prometheus-stack prometheus/kube-prometheus-stack \
+  --version $KUBE_PROMETHEUS_STACK_VERSION -f helm/values/kube-prometheus-stack.yaml \
+  -n monitoring --create-namespace
+
+# metallb
+METALLB_VERSION=0.12.1 
+
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
-helm upgrade --install metallb metallb/metallb -f helm/values/metallb.yaml -n kube-system
+helm upgrade --install metallb metallb/metallb \
+  --version $METALLB_VERSION -f helm/values/metallb.yaml \
+  -n kube-system
