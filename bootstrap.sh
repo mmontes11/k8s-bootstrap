b8f523c528bc7390314aecb62c8ffa1b097e0dae
Martín Montes <martin11lrx@gmail.com>
2022-04-28 22:40:01 +0200

    Added coredns config

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,11 @@ kubectl label node k8s-worker0 node.mmontes.io/type=compute
 CILIUM_VERSION=1.11.4 
 helm repo add cilium https://helm.cilium.io/
 helm upgrade --install cilium cilium/cilium --version $CILIUM_VERSION -n kube-system
-
 cilium status --wait
+
+# coredns
+kubectl apply -f manifests/configmaps/coredns.yaml
+kubectl rollout restart deployment/coredns -n kube-system
 
 # local path provisioner
 helm upgrade --install local-path-provisioner ./helm/charts/local-path-provisioner -n kube-system
