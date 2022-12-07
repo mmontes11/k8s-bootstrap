f590582702d287a5e7cbc4b480721feb3ec11018
Martín Montes <martin11lrx@gmail.com>
2022-12-07 12:35:51 +0100

    Moved sealed secrets key to secrets namespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,35 +12,39 @@ if [ -z "$GITHUB_REPO" ]; then
   exit 1
 fi
 
-# nodes
-# source ./scripts/nodes.sh
+nodes
+source ./scripts/nodes.sh
 
-# certificate signing requests
-# kubectl get csr \
-#   -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' \
-#   | xargs kubectl certificate approve
+certificate signing requests
+kubectl get csr \
+  -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' \
+  | xargs kubectl certificate approve
 
-# # cilium
-# CILIUM_VERSION=1.11.4 
-# helm repo add cilium https://helm.cilium.io/
-# helm upgrade --install \
-#   cilium cilium/cilium --version $CILIUM_VERSION \
-#   -f helm/values/cilium.yaml \
-#   -n kube-system
-# cilium status --wait
+# cilium
+CILIUM_VERSION=1.11.4 
+helm repo add cilium https://helm.cilium.io/
+helm upgrade --install \
+  cilium cilium/cilium --version $CILIUM_VERSION \
+  -f helm/values/cilium.yaml \
+  -n kube-system
+cilium status --wait
 
-# # local path provisioner
-# helm upgrade --install local-path-provisioner ./helm/charts/local-path-provisioner -n kube-system
+# local path provisioner
+helm upgrade --install local-path-provisioner ./helm/charts/local-path-provisioner -n kube-system
 
 # sealed secrets
+SECRETS_NAMESPACE=secrets
+kubectl create namespace $SECRETS_NAMESPACE
 kubectl create secret tls \
-  -n kube-system sealed-secrets-key \
+  -n $SECRETS_NAMESPACE \
+  sealed-secrets-key \
   --cert=certs/tls.crt --key=certs/tls.key \
   --dry-run=client -o yaml \
   | kubectl apply -f -
 kubectl label secret \
-  -n kube-system sealed-secrets-key \
-   sealedsecrets.bitnami.com/sealed-secrets-key=active \
+  -n $SECRETS_NAMESPACE \
+  sealed-secrets-key \
+  sealedsecrets.bitnami.com/sealed-secrets-key=active \
   --overwrite 
 
 # flux 
