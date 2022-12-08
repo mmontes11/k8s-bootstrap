b37cb07d1bff9a9bb5b4decab92d7477c9d7de67
Martín Montes <martin11lrx@gmail.com>
2022-12-08 16:04:41 +0100

    Updated containerd options

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -6,9 +6,8 @@ KUBERNETES_VERSION=1.25.4-00
 
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
-  | sudo tee /etc/apt/sources.list.d/kubernetes.list
+  | tee /etc/apt/sources.list.d/kubernetes.list
 
-cp config/crictl.yaml /etc/crictl.yaml
 mkdir -p /etc/kubernetes/manifests
 
 apt update
