0652158ca5592ac40ff58345876ff8ebc5578fa5
Martín Montes <martin11lrx@gmail.com>
2022-12-08 13:26:41 +0100

    Bump kubernetes version

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -2,18 +2,14 @@
 
 set -euo pipefail
 
-KUBERNETES_VERSION=1.22.9-00 
-
-# crictl
-cp config/crictl.yaml /etc/crictl.yaml
-
-# kubelet
-mkdir -p /etc/kubernetes/manifests
+KUBERNETES_VERSION=1.25.4-00
 
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
   | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+cp config/crictl.yaml /etc/crictl.yaml
+mkdir -p /etc/kubernetes/manifests
 
 apt update
 apt install -y kubelet=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION
