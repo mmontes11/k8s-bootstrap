a64e93e26e1250d8e20f97f1d5d733bbff90d6cd
Martín Montes <martin11lrx@gmail.com>
2022-12-08 13:27:49 +0100

    Loaded kernel modules required for ipvs

--- scripts/network.sh
+++ scripts/network.sh
@@ -2,27 +2,51 @@
 
 set -euo pipefail
 
-cat <<EOF | tee /etc/modules-load.d/kubernetes.conf
+source ./scripts/lib.sh
+
+# see:
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+# https://github.com/kubernetes/kubernetes/blob/master/pkg/proxy/ipvs/README.md#prerequisite
+
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
+ip_vs
+ip_vs_rr
+ip_vs_wrr
+ip_vs_sh
+nf_conntrack
 EOF
 
-modprobe overlay
-modprobe br_netfilter
+modules=(
+  overlay
+  br_netfilter
+  ip_vs
+  ip_vs_rr
+  ip_vs_wrr
+  ip_vs_sh
+  nf_conntrack
+)
 
-cat <<EOF | tee /etc/sysctl.d/kubernetes.conf
+for i in "${!modules[@]}"; do
+  modprobe -- "${modules[$i]}"
+done
+
+lsmod | grep -e overlay -e br_netfilter -e ip_vs -e nf_conntrack
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 sysctl --system
 
 # local hosts
-cat <<EOT >> /etc/hosts
-192.168.0.100 k8s-master.local
-192.168.0.101 k8s-worker0.local
-192.168.0.102 k8s-worker1.local
-192.168.0.103 k8s-worker2.local
+cat <<EOT | tee /etc/hosts
+192.168.0.110 k8s-master.local
+192.168.0.111 k8s-worker0.local
+192.168.0.112 k8s-worker1.local
 192.168.0.120 nas.local
 EOT
