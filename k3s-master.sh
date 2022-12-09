53b39438b912be678671617a142d231d0f8c5c75
Martín Montes <martin11lrx@gmail.com>
2022-12-09 23:35:21 +0100

    Config fixes

--- k3s-master.sh
+++ k3s-master.sh
@@ -6,7 +6,7 @@ mkdir -p /etc/rancher/k3s
 cp config/k3s.yaml /etc/rancher/k3s/config.yaml
 
 export INSTALL_K3S_VERSION=v1.25.4+k3s1
-curl -sfL https://get.k3s.io | sh -s -
+curl -sfL https://get.k3s.io | sh -
 
-mkdir ~/.kube
-cp /etc/rancher/k3s/config.yaml ~/.kube/config
+mkdir -p /home/mmontes/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/mmontes/.kube/config
