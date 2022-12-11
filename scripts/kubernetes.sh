056fe61ef189a8ea7b35f81e3890cfeb16371222
Martin Montes <martin11lrx@gmail.com>
2022-12-11 20:45:33 +0100

    Updated to kubernetes 1.26

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-KUBERNETES_VERSION=1.25.4-00
+KUBERNETES_VERSION=1.26.0-00
 
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
@@ -12,5 +12,5 @@ cp config/crictl.yaml /etc/crictl.yaml
 mkdir -p /etc/kubernetes/manifests
 
 apt update
-apt install -y kubelet=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION
+apt install -y --allow-change-held-packages kubelet=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION
 apt-mark hold kubelet kubeadm kubectl
