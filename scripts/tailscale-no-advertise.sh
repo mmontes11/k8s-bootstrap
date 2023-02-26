ee74c72eba5907fa40204b3f352dcb5402a26201
Martin Montes <martin11lrx@gmail.com>
2023-02-26 13:45:07 +0100

    Not advertising CIDR in woker

---
+++ scripts/tailscale-no-advertise.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+
+apt update
+apt install -y tailscale
+
+tailscale up --accept-dns=false --accept-routes=false --reset
+tailscale status
