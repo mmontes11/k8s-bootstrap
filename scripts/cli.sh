db88e3be7255fb008d99d9cbf0a2e7374d9ff276
Martin Montes <martin11lrx@gmail.com>
2024-05-04 16:54:08 +0200

    Bump k8s-scripts

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -27,5 +27,5 @@ function setup_kubeconfig() {
 setup_kubeconfig root /root
 setup_kubeconfig $(get_user) $(get_user_home)
 
-KUBE_SCRIPTS_VERSION=v0.0.1
-curl -sfL https://raw.githubusercontent.com/mmontes11/k8s-scripts/$KUBE_SCRIPTS_VERSION/kubernetes.sh | bash -s -
+K8S_SCRIPTS_VERSION=v0.0.19
+curl -sfL https://raw.githubusercontent.com/mmontes11/k8s-scripts/$K8S_SCRIPTS_VERSION/kubernetes.sh | bash -s -
