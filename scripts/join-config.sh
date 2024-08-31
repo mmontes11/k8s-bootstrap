41c84596d14bcd1d9c4e7cf70ff0d8df1e7da13c
Martin Montes <martin11lrx@gmail.com>
2024-08-31 19:55:48 +0200

    Generating join configs with kubeadm-join-config

--- scripts/join-config.sh
+++ scripts/join-config.sh
@@ -3,21 +3,42 @@
 set -euo pipefail
 
 API_SERVER_ENDPOINT=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}' | sed s/'http[s]\?:\/\/'//)
-TOKEN=$(kubeadm token list -o jsonpath='{.token}')
+TOKEN=$(kubeadm token create)
 CA_CERT_HASH="sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl ec -pubin -outform der 2>/dev/null | sha256sum | cut -d' ' -f1)"
 
 echo "Generating kubeadm-join.yaml config..."
 
-cat config/kubeadm-join.tpl.yaml | \
-  sed "s/{{API_SERVER_ENDPOINT}}/$API_SERVER_ENDPOINT/g" | \
-  sed "s/{{TOKEN}}/$TOKEN/g" | \
-  sed "s/{{CA_CERT_HASH}}/$CA_CERT_HASH/g" > config/kubeadm-join.yaml
+kubeadm-join-config --config-path ./config/kubeadm-join.compute.yaml \
+  --api-server-endpoint $API_SERVER_ENDPOINT \
+  --token $TOKEN \
+  --ca-cert-hash $CA_CERT_HASH \
+  --label-key node.mmontes.io/type \
+  --label-value compute
+
+kubeadm-join-config --config-path ./config/kubeadm-join.compute-small.yaml \
+  --api-server-endpoint $API_SERVER_ENDPOINT \
+  --token $TOKEN \
+  --ca-cert-hash $CA_CERT_HASH \
+  --label-key node.mmontes.io/type \
+  --label-value compute-small
+
+kubeadm-join-config --config-path ./config/kubeadm-join.storage.yaml \
+  --api-server-endpoint $API_SERVER_ENDPOINT \
+  --token $TOKEN \
+  --ca-cert-hash $CA_CERT_HASH \
+  --label-key node.mmontes.io/type \
+  --label-value storage \
+  --taint node.mmontes.io/storage
 
 cat << EOF
 
-Generated join configuration file: 'config/kubeadm-join.yaml'
-Copy this file to each worker node and run this command to join the cluster:
+Generated join configuration files: 
+- 'config/kubeadm-join.compute.yaml'
+- 'config/kubeadm-join.compute-small.yaml'
+- 'config/kubeadm-join.storage.yaml'
 
-sudo bash worker.sh
+Copy these file to each worker node and run this command to join the cluster:
+
+sudo bash node.sh 'config/kubeadm-join.compute.yaml'
 
 EOF
