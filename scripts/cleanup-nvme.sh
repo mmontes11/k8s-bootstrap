38b3ba026b0132099b937abf0026a584a7d9fa04
Martin Montes <martin11lrx@gmail.com>
2024-09-01 12:49:13 +0200

    Added cleanup nvme script

---
+++ scripts/cleanup-nvme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK="/dev/nvme0n1"
+
+# Zap the disk to a fresh, usable state (zap-all is important, b/c MBR has to be clean)
+sgdisk --zap-all $DISK
+
+# Wipe a large portion of the beginning of the disk to remove more LVM metadata that may be present
+dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
+
+# SSDs may be better cleaned with blkdiscard instead of dd
+blkdiscard $DISK
+
+# Inform the OS of partition table changes
+partprobe $DISK
