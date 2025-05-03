90a287e786f2cda19f586bb23f8f32cec2a61073
Martin Montes <martin11lrx@gmail.com>
2025-05-03 22:40:00 +0200

    Added nvme/pcie parameters

--- node-prepare.sh
+++ node-prepare.sh
@@ -2,16 +2,23 @@
 
 set -euo pipefail
 
+#!/bin/bash
+
+# Kubernetes-required cgroup parameters:
 if grep -q "cgroup_enable=cpuset" /boot/firmware/cmdline.txt &&
    grep -q "cgroup_enable=memory" /boot/firmware/cmdline.txt &&
    grep -q "cgroup_memory=1" /boot/firmware/cmdline.txt &&
    grep -q "cgroup_enable=hugetlb" /boot/firmware/cmdline.txt &&
-   grep -q "cgroup_enable=blkio" /boot/firmware/cmdline.txt; then
+   grep -q "cgroup_enable=blkio" /boot/firmware/cmdline.txt &&
+   # NVMe/PCIe stability parameters:
+   grep -q "nvme_core.default_ps_max_latency_us=0" /boot/firmware/cmdline.txt &&
+   grep -q "pcie_aspm=off" /boot/firmware/cmdline.txt &&
+   grep -q "pcie_port_pm=off" /boot/firmware/cmdline.txt; then
 
    echo "Boot parameters already set"
 else
-   echo " cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 cgroup_enable=hugetlb cgroup_enable=blkio" \
-    | tee -a /boot/firmware/cmdline.txt >/dev/null
+   echo "Appending boot parameters..."
+   sed -i 's|$| cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 cgroup_enable=hugetlb cgroup_enable=blkio nvme_core.default_ps_max_latency_us=0 pcie_aspm=off pcie_port_pm=off|' /boot/firmware/cmdline.txt
 
    echo "Boot parameters added"
 fi
