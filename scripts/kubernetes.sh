e209eb93eb39b9ebd89c5669c52cf58bd9527f27
Martin Montes <martin11lrx@gmail.com>
2025-09-14 14:18:17 +0200

    Updated kubernetes to v1.34.1

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # see:
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
 
-KUBERNETES_VERSION=v1.33
-KUBERNETES_PKG=1.33.4-1.1
+KUBERNETES_VERSION=v1.34
+KUBERNETES_PKG=1.34.1-1.1
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBERNETES_VERSION}/deb/Release.key \
   | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -20,5 +20,4 @@ apt update
 apt install -y --allow-change-held-packages kubelet=${KUBERNETES_PKG} kubeadm=${KUBERNETES_PKG} kubectl=${KUBERNETES_PKG}
 apt-mark hold kubelet kubeadm kubectl
 
-# optional: enable the kubelet service before running kubeadm
-# systemctl enable --now kubelet
+systemctl enable --now kubelet
